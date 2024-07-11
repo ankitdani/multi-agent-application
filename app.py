58e2458e364adaddc162cda534dd09a27f7f2e9b
ankitdani <ankitdani1997@gmail.com>
2024-07-10 22:57:29 -0700

    Created user input using flask

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/home', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Handle form submission here
+        job_role = request.form['job_role']
+        years_of_experience = request.form['years_of_experience']
+        return f'Job Role: {job_role}, Years of Experience: {years_of_experience}'
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
